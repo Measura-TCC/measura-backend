aa7d87c9ac61f5fba8aad4b9e7cad2540e41d3c1
João Victor Ferreira <joaovictor2111@gmail.com>
2025-10-26 18:09:02 -0300

    chore: remove unnecessary files

--- list-github-repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source .env
-
-curl -s "https://api.github.com/user/repos?per_page=100&sort=updated" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" | \
-  jq -r '.[] | "\(.full_name) - Open Issues: \(.open_issues_count) - Has Issues: \(.has_issues) - Private: \(.private)"'
